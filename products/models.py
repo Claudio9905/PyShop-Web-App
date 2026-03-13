8be49bee2f0cfdf27679833f6ac691ecf785f687
Claudio9905 <cloudpost10@gmail.com>
2026-03-13 17:08:17 +0100

    Update File

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     stock = models.IntegerField()
-    image_url = models.CharField(max_length=2083)
+    image = models.ImageField(upload_to='products/')
 
 
 class Offer(models.Model):
